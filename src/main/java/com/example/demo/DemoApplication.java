9682c90835b09cb25ab6baab99ba03d0a0cae504
Taylor Cook <tecook2@mail.fhsu.edu>
2021-09-23 10:21:18 -0500

    testing persistance

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
-		return "Use the api by hitting /recipes, /add-recipe, /delete-recipe/{id}, /update-recipe/{id}, /recipe/{id}";
+		return "Use the API by hitting /recipes, /add-recipe, /delete-recipe/{id}, /update-recipe/{id}, /recipe/{id}";
 	}
 
 	public static void main(String[] args) {
